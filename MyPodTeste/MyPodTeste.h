347b86c5f8acb2d84cdece30f7458bac081587b3
Rodolfo Silva <reedysilvabr@gmail.com>
2023-02-07 00:27:57 -0300

    first commit

---
+++ MyPodTeste/MyPodTeste.h
@@ -0,0 +1,18 @@
+//
+//  MyPodTeste.h
+//  MyPodTeste
+//
+//  Created by rodolfo silva on 07/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyPodTeste.
+FOUNDATION_EXPORT double MyPodTesteVersionNumber;
+
+//! Project version string for MyPodTeste.
+FOUNDATION_EXPORT const unsigned char MyPodTesteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyPodTeste/PublicHeader.h>
+
+
